c427c87efcfdd8c9f4c28ec990f9cf249902764c
Giuseppe Cito <giuseppe.cito48@gmail.com>
2024-05-29 20:30:29 +0200

    buttons

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ButtonsComponent } from '../../components/buttons/buttons.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,ButtonsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
