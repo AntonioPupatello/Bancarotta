c4915e6e66c7e70347fc8645389a71e5b59869a9
Antonio <pupatello.antonio.97@gmail.com>
2024-05-29 14:31:12 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { LayoutComponent } from './components/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SigninComponent } from './pages/signin/signin.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, LayoutComponent, HomeComponent, LoginComponent, SigninComponent],
+  imports: [RouterOutlet, RouterModule, LayoutComponent, HomeComponent, LoginComponent, SigninComponent, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
