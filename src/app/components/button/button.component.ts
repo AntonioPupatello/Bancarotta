909a473ba9713a41df1786742db5fce262e1476d
Antonio <pupatello.antonio.97@gmail.com>
2024-05-31 14:29:29 +0200

    update

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 /**
  * @title Basic buttons
@@ -10,7 +11,7 @@ import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, RouterModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
