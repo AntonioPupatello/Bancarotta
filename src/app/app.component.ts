963860d127a03fa42e2766cac4d8ecd8d3d30247
Giuseppe Cito <giuseppe.cito48@gmail.com>
2024-06-01 20:35:46 +0200

    Antonio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
-import { LayoutComponent } from './components/layout/layout.component';
-import { LoginComponent } from './pages/login/login.component';
-import { SigninComponent } from './pages/signin/signin.component';
-import { ButtonsComponent } from './components/buttons/buttons.component';
-import { MatButtonModule } from '@angular/material/button';
-import { CommonModule, NgIf } from '@angular/common';
-=======
->>>>>>> origin/antonio
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
-  imports: [RouterOutlet, RouterModule, LayoutComponent, LoginComponent, SigninComponent,ButtonsComponent,MatButtonModule,CommonModule,NgIf],
-=======
+
   imports: [RouterOutlet, RouterModule],
->>>>>>> origin/antonio
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'bank-project';
-  isLoginVisible: boolean = false;
-  isSigninVisible: boolean = false;
 
-  showLogin() {
-    this.isLoginVisible = true;
-    this.isSigninVisible = false;
-  }
-
-  showSignin() {
-    this.isLoginVisible = false;
-    this.isSigninVisible = true;
-  }
 }
