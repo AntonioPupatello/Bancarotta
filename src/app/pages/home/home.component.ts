0b6f765b3ee82467ee6a6fcfa16ed8347a1e9a04
Antonio <pupatello.antonio.97@gmail.com>
2024-06-02 16:17:52 +0200

    moltobene

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,13 @@ import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { LayoutComponent } from '../../components/layout/layout.component';
+import { ContactComponent } from '../contact/contact.component';
+import { TabGroupBasicExample } from '../../components/tabs/tabs.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, MatButtonModule, MatToolbarModule, LayoutComponent],
+  imports: [RouterModule, MatButtonModule, MatToolbarModule, LayoutComponent, ContactComponent, TabGroupBasicExample],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -5,11 +5,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { RouterModule } from '@angular/router';
 import { LayoutComponent } from '../../components/layout/layout.component';
+import { StepperEditableExample } from '../../components/stepper/stepper.component';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [RouterModule, FormsModule, MatInputModule, MatFormFieldModule, MatButtonModule, FormsModule, LayoutComponent],
+  imports: [RouterModule, FormsModule, MatInputModule, MatFormFieldModule, MatButtonModule, FormsModule, LayoutComponent,StepperEditableExample ],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss'
 })
