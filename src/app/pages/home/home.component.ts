5cfa052192df4ab41bb5ee20df83e0abb65f7eaf
Antonio <pupatello.antonio.97@gmail.com>
2024-06-02 12:43:54 +0200

    vabene

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { LoginComponent } from '../login/login.component';
-import { ButtonComponent } from '../../components/button/button.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { LayoutComponent } from '../../components/layout/layout.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, MatButtonModule, MatToolbarModule],
+  imports: [RouterModule, MatButtonModule, MatToolbarModule, LayoutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutComponent } from '../../components/layout/layout.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, MatInputModule, MatFormFieldModule, MatButtonModule, FormsModule, LayoutComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  onSubmit(form: NgForm) {
+    const email = form.value.email
+    const password = form.value.password}
 }
