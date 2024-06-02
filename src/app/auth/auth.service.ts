85d97a1c5d4b00de49e4cb10719bac5d91e59cf0
Giuseppe Cito <giuseppe.cito48@gmail.com>
2024-06-02 14:11:06 +0200

    login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor() { }
+LoginUrl = ''
+  constructor(private http:HttpClient) { }
+  Login(email: string, password: string){
+    return this.http.post(this.LoginUrl,{email: email, password: password, returnSecureToken: true})
+  }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,FormsModule,MatFormFieldModule,MatInputModule,MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+constructor(private authService: AuthService){}
+
+onSubmit(form: NgForm) {
+const email = form.value.email
+const password = form.value.password
+this.authService.Login(email,password).subscribe(data =>{
+  console.log(data)
+})
+}
 
 }
