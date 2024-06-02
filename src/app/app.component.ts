177052d49e1fb112ed8deaf27961e00d64f36bc6
Simone <simoneurbani.lavoro@gmail.com>
2024-06-02 20:10:49 +0200

    #

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 import { LayoutComponent } from './components/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SigninComponent } from './pages/signin/signin.component';
-import { ButtonComponent } from './components/button/button.component';
-=======
->>>>>>> antonio
 
 @Component({
   selector: 'app-root',
@@ -15,11 +11,7 @@ import { ButtonComponent } from './components/button/button.component';
 <<<<<<< HEAD
   imports: [RouterOutlet, RouterModule, LayoutComponent, HomeComponent, LoginComponent, SigninComponent, ButtonComponent],
 =======
-  imports: [RouterOutlet, RouterModule],
 >>>>>>> antonio
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-})
 export class AppComponent {
   title = 'bank-project';
 }
