0b8c9d238dcdb43887537b20b57ac59319bd3e7c
giuseppe <giuseppe.cito48@gmail.com>
2024-06-05 13:47:39 +0200

    feat: transfer

--- src/app/pages/banktransfer/banktransfer.component.ts
+++ src/app/pages/banktransfer/banktransfer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { LayoutComponent } from '../../components/layout/layout.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { BanktransferService } from '../../services/banktransfer.service';
+import { Transfer } from '../../models/transfer.model';
 
 @Component({
   selector: 'app-banktransfer',
@@ -14,9 +15,52 @@ import { BanktransferService } from '../../services/banktransfer.service';
   styleUrl: './banktransfer.component.scss'
 })
 export class BanktransferComponent {
-onSubmit(form: NgForm) {
+  transferForm: FormGroup 
+onSubmit() {
+  if (this.transferForm.valid) {
+    const sender: Sender = this.transferForm.value.sender;
+    const recipient: Recipient = this.transferForm.value.recipient;
+    const details: TransactionDetails = this.transferForm.value.details;
+    const security: Security = this.transferForm.value.security;
 
-}
-constructor(private bankTransfer:BanktransferService){}
+    const transaction = new Transfer(sender, recipient, details, security);
 
+    this.bankTransferService.transfer(transaction).subscribe(
+      response => {
+        console.log('Trasferimento effettuato con successo!', response);
+      },
+      error => {
+        console.error('Errore nel trasferimento: ', error);
+      }
+    );
+  }
 }
+}
+
+constructor(private bankTransfer:BanktransferService, private fb: FormBuilder){
+  this.transferForm = this.fb.group({
+  sender: this.fb.group({
+    name: ['', Validators.required],
+    iban: ['', Validators.required],
+    bank: ['', Validators.required]
+  }),
+  recipient: this.fb.group({
+    name: ['', Validators.required],
+    iban: ['', Validators.required],
+    bank: ['', Validators.required],
+    bic: ['', Validators.required]
+  }),
+  details: this.fb.group({
+    amount: ['', Validators.required],
+    currency: ['EUR', Validators.required],
+    description: ['', Validators.required],
+    execution_date: ['', Validators.required]
+  }),
+  security: this.fb.group({
+    auth_token: ['', Validators.required]
+   
+  })
+});
+}
+
+
